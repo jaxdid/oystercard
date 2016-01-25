0ff01e8892f0eea2d471a54755c07d9935c9f648
Alain Lemaire <jaxdid@gmail.com>
2016-01-25 17:49:26 +0100

    Tested for OysterCard#balance to initially be set to 0. Added appropriate OysterCard instance methods.

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,17 @@
-describe Oystercard do
+require 'oystercard'
+
+describe OysterCard do
+  subject(:card) { OysterCard.new }
   
+  describe '#add' do
+    it 'responds to #add with one argument' do
+      expect(card).to respond_to(:add).with(1).argument
+    end
+  end
+
+  describe '#balance' do
+    it 'returns a balance' do
+      expect(card.balance).to eq 0
+    end
+  end
 end
